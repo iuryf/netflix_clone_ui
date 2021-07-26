a02172d5d786ef26737b370f729c801c5f9336bc
Iury Farias <iurymfarias@gmail.com>
2021-07-26 12:03:42 -0300

    commit start

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './Header.css';
 
-export default () => {
+export default ({black}) => {
     return (
-          <header>
+          <header className={black ? 'black' : ''}>
               <div className="header--logo">
                   <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg?uselang=pt" alt="Netflix" />
               </div>
               <div className="header--user">
                   <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?uselang=pt" alt="Usuário"/>
               </div>
-              <div>
-
-              </div>
           </header>    
-    );
+    )
 }
