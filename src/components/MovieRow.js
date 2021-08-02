ca8ce47eb7db3b6146d8cf403e037248e61bccb4
Iury Farias <iurymfarias@gmail.com>
2021-08-02 16:30:50 -0300

    commit start

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -5,17 +5,27 @@ import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 
 export default ({title, items}) => {
+    const handleLeftArrow = () => {
+
+    }
+
+    const handleRightArrow = () => {
+
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
-            <div className="movieRow--left">
+            <div className="movieRow--left" onClick={handleLeftArrow}>
                 <NavigateBeforeIcon style={{fontSize: 50}} />
             </div>
-            <div className="movieRow--right">
+            <div className="movieRow--right" onClick={handleRightArrow}>
                 <NavigateNextIcon style={{fontSize: 50}} />
             </div>
             <div className="movieRow--listarea">
-                <div className="movieRow--list">
+                <div className="movieRow--list" style={{
+                    marginLeft: 0
+                }}>
                {items.results.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className="movieRow--item">    
                             <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
