1c53d32320c270c4a428da84a50ff87bf052558b
Iury Farias <iurymfarias@gmail.com>
2021-08-04 10:04:15 -0300

    commit start

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -5,12 +5,23 @@ import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 
 export default ({title, items}) => {
-    const handleLeftArrow = () => {
+    const [scrollX, setScrollX] = useState(0)
 
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if (x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
     }
 
     const handleRightArrow = () => {
-
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 150;
+        if ((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
     }
 
     return (
@@ -24,9 +35,10 @@ export default ({title, items}) => {
             </div>
             <div className="movieRow--listarea">
                 <div className="movieRow--list" style={{
-                    marginLeft: 0
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
                 }}>
-               {items.results.length > 0 && items.results.map((item, key)=>(
+                    {items.results.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className="movieRow--item">    
                             <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
                         </div>
